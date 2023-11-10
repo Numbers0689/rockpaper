dbf0f49854d19b680535873fac98ae744d5a8451
Numbers0689 <firewatchsai@gmail.com>
2023-11-10 23:28:07 +0530

    Add other needed functions
    playround, playerchoice, cmpchoice etc.

--- script.js
+++ script.js
@@ -4,12 +4,66 @@ function getComputerChoice()
     switch(Math.floor(Math.random() * 3))
     {
         case 0:
-            return "rock";
+            return "Rock";
         case 1:
-            return "paper";
+            return "Paper";
         case 2:
-            return "scissors";
+            return "Scissors";
     }
 }
 
-console.log(getComputerChoice())
+let playerSelection = prompt("Rock paper scissors: ");
+let computerSelection = getComputerChoice();
+let cscore = 0;
+let pscore = 0;
+
+function playRound(computerSelection, playerSelection) 
+{
+    let c = computerSelection.toLowerCase();
+    let p = playerSelection.toLowerCase();
+    if (c === p)
+    {
+        return "Its a draw";
+    }
+    else if (c === "rock" && p === "paper")
+    {
+        pscore ++;
+        return "You win! paper beats rock.";
+    }
+    else if (c === "rock" && p === "scissors")
+    {
+        cscore ++;
+        return "You lose! rock beats scissors.";
+    }
+    else if (c === "paper" && p === "scissors")
+    {
+        pscore ++;
+        return "You win! scissors beats paper.";
+    }
+    else if (c === "paper" && p === "rock")
+    {
+        cscore ++;
+        return "You lose! paper beats rock.";
+    }
+    else if (c === "scissors" && p === "paper")
+    {
+        cscore ++;
+        return "You lose! scissors beats paper.";
+    }
+    else if (c === "scissors" && p === "rock")
+    {
+        pscore ++;
+        return "You win! rock beats scissors.";
+    }
+}
+
+function game()
+{
+    for(let i = 0; i < 5; i++)
+    {
+        console.log(playRound(computerSelection, playerSelection));
+
+    }
+}
+
+console.log(`${playerSelection} vs ${computerSelection}`);
